a11d161c3773a787f4277e47efd03243b850c57e
indraadi01 <mindraadikusuma@mail.ugm.ac.id>
2023-10-15 13:49:44 +0700

    Original Project

---
+++ app/src/main/java/com/example/acara06pgpb/ThirdActivity.java
@@ -0,0 +1,43 @@
+package com.example.acara06pgpb;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class ThirdActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_third);
+
+        // Mengambil data extra dari Intent
+        Intent intent = getIntent();
+        String usernameview = intent.getStringExtra("username");
+
+
+        // Tampilkan data pada TextView
+        TextView textView = findViewById(R.id.usernameView);
+        textView.setText("Welcome " +"" + usernameview);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_options, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId()== R.id.logout){
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
